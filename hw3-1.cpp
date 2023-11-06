aac227c46eb3b0dff8a3319951d2ecab651ebe3a
RayHuang <adopcarry@gmail.com>
2023-11-06 11:34:08 +0800

    DO hw3-1

---
+++ hw3-1.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+
+using namespace std;
+
+condition_variable cv;
+int turn = 0;
+
+void count(int index) {
+  static mutex io_mutex;
+  unique_lock<mutex> lock(io_mutex);
+  int num = 1000000;
+  
+  while (num--) {}
+  {
+    // use condition variable and turn to check which thread should output
+    cv.wait(lock, [&] { return index == turn; });
+
+    cout << "I'm thread " << index << ", local count: 1000000\n";
+    turn+=1;
+    
+    cv.notify_all();
+  }
+  
+}
+
+int main(void) {
+  thread t[100];
+  
+  for (int i = 0; i < 100; i++){
+    t[i] = thread(count, i);
+  }
+
+  for (int i = 0; i < 100; i++){
+    t[i].join();
+  }
+  
+}
